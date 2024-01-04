8b3dd39efdde2cbf41286d12ad75b35d714dbdac
David Cardoso <david.cardoso@mediaweb.pt>
2024-01-04 16:54:28 +0000

    limit navbar for login and logout user

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,11 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from datetime import date
+from website.models import Utilizador
+from website import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
+dataHoje = date.today()
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -17,19 +22,26 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        Nome = request.form.get('Nome')
+        nome = request.form.get('nome')
+        # dataNascimento = request.form.get('dataNascimento')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='erro')
-        elif len(Nome) < 2:
+        elif len(nome) < 2:
             flash('O nome tem que ter pelo menos duas letras.', category='erro')
+        # elif dataHoje - dataNascimento < 18:
+        # flash( 'a idade tem que ser superior a 18 anos', category='erro')
         elif password1 != password2:
             flash('A password não coíncide ', category='erro')
         elif len(password1) < 7:
             flash('A password tem que ter pelo menos 7 caractéres.', category='erro')
         else:
+            new_user = Utilizador(email=email, nome=nome, password=generate_password_hash(password1,method='scrypt'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('A conta foi criada com sucesso', category='sucesso')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
