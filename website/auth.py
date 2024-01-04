9c1471c135b0bd541731ee5431716c53989f824e
Bruno Roda <rodaeu@gmail.com>
2024-01-04 00:24:07 +0000

    mensagem de erro no sign up

--- website/auth.py
+++ website/auth.py
@@ -1,33 +1,35 @@
-from flask import Blueprint,render_template, request,flash
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
 
 
-auth = Blueprint('auth',__name__)
-
-@auth.route('/login', methods=['GET','POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
+
 
 @auth.route('/logout')
 def logout():
     return render_template("home.html")
 
-@auth.route('/sign-up',methods=['GET','POST'])
-def sign_up():
-    if request.method== 'POST':
-        email =request.form.get('email')
-        Nome = request.form.get('Nome')
-        password1=request.form.get('password1')
-        password2= request.form.get('password2')
 
-    if len(email) < 4:
-        flash('Email must be greater than 3 characters.', category='erro')
-    elif len(Nome)<2:
-        flash('O nome tem que ter pelo menos duas letras.', category='erro')
-    elif password1 != password2:
-        flash('A password não coíncide ', category='erro')
-    elif len(password1) < 7:
-        flash('A password tem que ter pelo menos 7 caractéres.', category='erro')
-    else:
-        flash('A conta foi criada com sucesso', category='sucesso')
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        Nome = request.form.get('Nome')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='erro')
+        elif len(Nome) < 2:
+            flash('O nome tem que ter pelo menos duas letras.', category='erro')
+        elif password1 != password2:
+            flash('A password não coíncide ', category='erro')
+        elif len(password1) < 7:
+            flash('A password tem que ter pelo menos 7 caractéres.', category='erro')
+        else:
+            flash('A conta foi criada com sucesso', category='sucesso')
 
     return render_template("sign_up.html")
