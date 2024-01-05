2a7e71c202d9fadad1a2186a667e4f7790d7bfd9
David Cardoso <david.cardoso@mediaweb.pt>
2024-01-05 19:37:03 +0000

    add notes (complete) remove notes(incomplete)

--- website/views.py
+++ website/views.py
@@ -1,11 +1,33 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 from flask_login import login_required,current_user
+from website.models import Notas
+from website import db
 
 
 views = Blueprint('views',__name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET','POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        notas = request.form.get('notas')
+
+        if len(notas) <1:
+            flash('Nota inixistente', category ='erro')
+        else:
+            nova_nota = Notas(data=notas, utilizador_id = current_user.id)
+            db.session.add(nova_nota)
+            db.session.commit()
+            flash('Nota criada', category='sucesso')
     return render_template("home.html",utilizador = current_user)
+
+@views.route('/', methods=['POST'])
+@login_required
+def eliminar():
+
+    notas = Notas.query.filter_by(utilizador_id = current_user.id ).delete()
+    db.session.commit()
+    flash('Nota Apagada', category='sucesso')
+
+    return render_template("home.html", utilizador=current_user)
