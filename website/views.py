491f963ec6e9eca961b70136d1672606d7607cc8
David Cardoso <david.cardoso@mediaweb.pt>
2024-01-10 20:04:24 +0000

    Alteração para reservas de carros

--- website/views.py
+++ website/views.py
@@ -1,37 +1,50 @@
-from flask import Blueprint,render_template,request,flash,redirect,url_for
-from flask_login import login_required,current_user
-from website.models import Notas,Utilizador
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from website.models import Reservas
 from website import db
 
 
-views = Blueprint('views',__name__)
+views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
-
-
 def home():
     if request.method == 'POST':
-        notas = request.form.get('notas')
+        reservas = request.form.get('reservas')
 
-        if len(notas) <1:
-            flash('Nota inixistente', category ='erro')
+        if len(reservas) < 1:
+            flash('Nota inixistente', category='erro')
         else:
-            nova_nota = Notas(data=notas, utilizador_id = current_user.id)
-            db.session.add(nova_nota)
+            nova_reserva = Reservas(data=reservas, utilizador_id=current_user.id)
+            db.session.add(nova_reserva)
             db.session.commit()
             flash('Nota criada', category='sucesso')
-    return render_template("home.html",utilizador = current_user)
+    return render_template("home.html", utilizador=current_user)
 
 
-@views.route('/eliminar', methods=['POST'])
+@views.route('/eliminar/<id>', methods=['POST'])
 @login_required
-def eliminar():
+def eliminar(id):
 
     if request.method == 'POST':
-        notas = Notas.query.filter_by(id=Notas.id).delete()
+        Reservas.query.filter_by(id=id).delete()
         db.session.commit()
         flash('Nota Apagada', category='sucesso')
     return redirect(url_for('views.home'))
 
+
+@views.route('/rent-a-car', methods=['GET', 'POST'])
+@login_required
+def rent():
+    if request.method == 'POST':
+        reservas = request.form.get('reservas')
+
+        if len(reservas) < 1:
+            flash('Nota inixistente', category='erro')
+        else:
+            nova_reserva = Reservas(data=reservas, utilizador_id=current_user.id)
+            db.session.add(nova_reserva)
+            db.session.commit()
+            flash('Nota criada', category='sucesso')
+    return render_template("rent-a-car.html", utilizador=current_user)
